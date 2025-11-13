ee3b7990ff0a2dae17afc504e690df7d7b317cfc
gkluz <gkluz@datagraphic.co.uk>
2025-11-13 14:18:27 +0000

    button and room

--- src/main/java/com/klusko/knowledge/controller/Main.java
+++ src/main/java/com/klusko/knowledge/controller/Main.java
@@ -1,5 +1,10 @@
 package com.klusko.knowledge.controller;
 
+import com.klusko.knowledge.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +13,24 @@ public class Main {
 
     @GetMapping("/")
     public String index() {
-        return "mainpage";
+        return "main";
+    }
+
+    @GetMapping("/room")
+    public String index2() {
+        return "room";
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return  chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
     }
 }
